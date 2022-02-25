14af1a3c811769c4f6975a2373b374dc50a6434a
elisabeth.droeser@gmail.com <elisabeth.droeser@gmail.com>
2022-02-25 14:47:53 +0100

    Add AutoMapper

--- Lms.Api/Program.cs
+++ Lms.Api/Program.cs
@@ -17,6 +17,7 @@ builder.Services.AddControllers(opt => opt.ReturnHttpNotAcceptable = true)
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(typeof(LmsApiContext));
 
 var app = builder.Build();
 
